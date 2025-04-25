52e4c27d63b93529a7e35d029bcb95f29eec9bda
MacLap.IN <chygautam02@gmail.com>
2025-04-25 16:56:30 +0530

    models added

--- rms/models.py
+++ rms/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+User = get_user_model()
 class Category(models.Model):
     name = models.CharField(max_length=100)
     
@@ -10,8 +13,45 @@ class Category(models.Model):
 class Food(models.Model):
      name = models.CharField(max_length=225)
      price = models.IntegerField()
-     Category = models.ForeignKey(Category,on_delete=models.CASCADE)
+     category = models.ForeignKey(Category,on_delete=models.CASCADE)
      
      def __str__(self):
          return self.name
      
+     
+class Table(models.Model):
+    number = models.IntegerField() 
+    available = models.BooleanField(default = True)  
+    
+    def __str__(self):
+        return f"Table No.{self.number}- {self.available}"  
+ 
+class Order(models.Model):
+    
+    PENDING = 'P'
+    COMPLETED = 'C'
+    STATUS_CHOICE = {
+        PENDING :"Pending",
+        COMPLETED :"Completed"
+    }
+    
+    PAID = 'P'
+    UNPAID = 'U'
+    PAYMENT_CHOICE ={
+        PAID : 'Paid',
+        UNPAID : 'Not paid'
+    }
+    user =  models.ForeignKey(User,on_delete=models.CASCADE) 
+    total_price = models.IntegerField()
+    status = models.CharField(max_length=1, choices=STATUS_CHOICE,default=PENDING)
+    payment_status = models.CharField(max_length=1,choices=PAYMENT_CHOICE,default=UNPAID)
+    
+    def __str__(self):
+      return f"{self.user} - {self.status}"
+  
+class OrderItem(models.Model):
+    food = models.ForeignKey(Food, on_delete=models.PROTECT)
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+        
+        
+        
